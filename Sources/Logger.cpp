b232716ef2a3e78bb4b44c91b74660325b4f9073
myevan <myevan@outlook.com>
2017-07-11 02:11:44 +0900

    ! logger singleton tempalte applied

--- Sources/Logger.cpp
+++ Sources/Logger.cpp
@@ -1,20 +1,5 @@
 #include "Logger.h"
 
-#include <assert.h>
-
 namespace EL
 {
-    Logger* Logger::_inst = nullptr;
-
-    Logger& Logger::Get()
-    {
-        assert(_inst != nullptr);
-        return *_inst;
-    }
-
-    void Logger::Set(Logger* inst)
-    {
-        assert(inst != nullptr);
-        _inst = inst;
-    }
 }
--- Sources/Logger.h
+++ Sources/Logger.h
@@ -1,19 +1,17 @@
 #pragma once
 
+#include "Singleton.h"
+
 namespace EL
 {
-    class Logger
+    class Logger : public Singleton<Logger>
     {
-    public:
-        static Logger& Get();
-        static void Set(Logger* inst);
-
-    private:
-        static Logger* _inst;
-
     public:
         virtual ~Logger() = default;
         virtual void Info(const char* text) = 0;
+
+    protected:
+        Logger() = default;
     };
 
 }
