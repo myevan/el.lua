77c8542608df7eae35bc3f242c96917b81b3c97a
myevan <myevan@outlook.com>
2017-07-11 02:10:59 +0900

    - lua:cfunction:Derived

--- Sources/LuaGlobalState.cpp
+++ Sources/LuaGlobalState.cpp
@@ -7,33 +7,6 @@ extern "C"
 
 namespace EL
 {
-    int Derived(lua_State* state)
-    {
-        // _0 = {_cls, params}[2]
-        if (!lua_checkstack(state, 2))
-        {
-            puts("WRONG ARGUMENTS");
-            return 0;
-        }
-
-        lua_pushvalue(state, 2);
-
-        // setmetatable(_0, cls)
-        lua_pushvalue(state, 1); lua_setmetatable(state, -2);
-
-        // _0.__index = cls
-        lua_pushliteral(state, "__index");
-        lua_pushvalue(state, 2);
-        lua_rawset(state, -3); 
-
-        // _0.__newindex = cls
-        lua_pushliteral(state, "__newindex");
-        lua_pushvalue(state, 2);
-        lua_rawset(state, -3); 
-
-        return 1;
-    }
-
     LuaGlobalState& LuaGlobalState::Get()
     {
         static LuaGlobalState s_inst;
@@ -45,9 +18,6 @@ namespace EL
     {
         luaL_openlibs(_state);
         luaopen_base(_state);
-
-        lua_pushcfunction(_state, Derived);
-        lua_setglobal(_state, "derived");
     }
 
     LuaGlobalState::~LuaGlobalState()
