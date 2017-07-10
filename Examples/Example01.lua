5a96a74ab7396928ea8f4bdf61b31e7e7832696c
myevan <myevan@outlook.com>
2017-07-11 02:20:47 +0900

    + lua:example:World, Character

--- Examples/Example01.lua
+++ Examples/Example01.lua
@@ -3,3 +3,8 @@ print(Logger.Info)
 print(Logger:Get())
 Logger:Get():Info("11")
 Logger:Get():Info("22")
+
+local kChar = 1
+local hChar = World:Get():CreateCharacter(kChar)
+print(hChar)
+Character:Get(hChar):DestroySelf()
