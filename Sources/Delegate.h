a68317f7d66f77b0f29feb98933d0ec8ba9f5e1a
myevan <myevan80@gmail.com>
2017-08-30 17:44:16 +0900

    delegate rewritten base on handle

--- Sources/Delegate.h
+++ Sources/Delegate.h
@@ -2,51 +2,119 @@
 
 #include <vector>
 #include <memory>
+#include <functional>
+#include <cassert>
 
 namespace EL
 {
-    template<typename ... ArgTypes>
-    class Delegate
+    class HandleManager
     {
-    public:
-        using Func = std::function<void(ArgTypes...)>;
-
-    public:
-        void Add(Func func)
-        {
-            m_funcs.push_back(func);
-        }
-
-        void Remove(Func func)
-        {
-            m_funcs.remove(func);
-        }
-
-        virtual void Run(ArgTypes... args)
-        {
-            for (const Func& func: m_funcs)
+        public:
+            uint32_t AllocHandle()
             {
-                func(args...);
+                uint32_t handle;
+                if (m_freeIndices.empty())
+                {
+                    handle = GenerateHandle(m_totalHandles.size());
+                    m_totalHandles.push_back(handle);
+                }
+                else
+                {
+                    handle = GenerateHandle(m_freeIndices.back());
+                    m_freeIndices.pop_back();
+                }
+                return handle;
             }
-        }
 
-    public:
-        void operator+=(Func func)
-        {
-            Add(func);
-        }
+            bool TryFreeHandle(uint32_t handle, uint32_t& outIndex)
+            {
+                outIndex = GetHandleIndex(handle);
+                if (m_totalHandles[outIndex] == handle)
+                {
+                    m_totalHandles[outIndex] = 0;
+                    m_freeIndices.push_back(outIndex);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
 
-        void operator-=(Func func)
-        {
-            Remove(func);
-        }
+            uint32_t GetHandleIndex(uint32_t handle)
+            {
+                return handle & 0xffffff;
+            }
 
-        void operator()(ArgTypes... args)
-        {
-            Run(args...);
-        }
+        private:
+            uint32_t GenerateHandle(size_t index)
+            {
+                static uint32_t s_seq = 1;
+                uint32_t handle = (s_seq << 24) | (index & 0xffffff);
+                s_seq += 2;
+                s_seq &= 0xff;
+                return handle;
+            }
 
-    private:
-        std::vector<Func> m_funcs;
+        private:
+            std::vector<uint32_t> m_totalHandles;
+            std::vector<uint32_t> m_freeIndices;
     };
+
+    template<typename ... ArgTypes>
+        class Delegate
+        {
+            public:
+                using Func = std::function<void(ArgTypes...)>;
+
+            public:
+                uint32_t Add(Func func)
+                {
+                    uint32_t handle = m_handleManager.AllocHandle();
+                    uint32_t index = m_handleManager.GetHandleIndex(handle);
+                    if (index < m_funcs.size())
+                    {
+                        m_funcs[index] = func;
+                    }
+                    else
+                    {
+                        m_funcs.push_back(func);
+                    }
+
+                    return handle;
+                }
+
+                void Remove(uint32_t handle)
+                {
+                    uint32_t index;
+                    if (m_handleManager.TryFreeHandle(handle, index))
+                    {
+                        assert(index < m_funcs.size());
+                        m_funcs[index] = nullptr;
+                    }
+                }
+
+                virtual void Run(ArgTypes... args)
+                {
+                    for (const Func& func: m_funcs)
+                    {
+                        if (func != nullptr)
+                        {
+                            func(args...);
+                        }
+                    }
+                }
+
+            public:
+                void operator()(ArgTypes... args)
+                {
+                    Run(args...);
+                }
+
+            private:
+                HandleManager m_handleManager;
+
+                std::vector<Func> m_funcs;
+
+        };
 }
